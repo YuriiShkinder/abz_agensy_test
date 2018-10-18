184d0a20ed3606056912173b2d928cacb1408b90
Yurii Shkinder <yurkaaa96@gmail.com>
2018-10-18 14:26:18 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ ! -f ./.env ]; then
 fi
 
 echo "run docker"
-docker-compose -p $nameFolder up --build -d &&
+docker-compose -p abzagensytest up --build -d &&
 
 docker run --rm --interactive --tty \
     --volume $PWD:/app \
@@ -15,15 +15,15 @@ docker run --rm --interactive --tty \
 
 
 echo "storage chmod 777"
-docker exec ${nameFolder}_web_1 chmod 777 -R storage/ &&
+docker exec abzagensytest_web_1 chmod 777 -R storage/ &&
 
 echo "php artisan key:generate"
-docker exec ${nameFolder}_web_1 php artisan key:generate &&
+docker exec abzagensytest_web_1 php artisan key:generate &&
 
 echo "create database abz if mot exists"
-docker exec -i ${nameFolder}_db_1 mysql -u abz --password=abz -e "CREATE DATABASE IF NOT EXISTS abz" &&
+docker exec -i abzagensytest_db_1 mysql -u abz --password=abz -e "CREATE DATABASE IF NOT EXISTS abz" &&
 
 echo "load dump  to database abz"
-docker exec -i ${nameFolder}_db_1 mysql -u abz --password=abz abz  < dump.sql
+docker exec -i abzagensytest_db_1 mysql -u abz --password=abz abz  < dump.sql
 
 echo "go to link: http://127.0.0.1:8080"
